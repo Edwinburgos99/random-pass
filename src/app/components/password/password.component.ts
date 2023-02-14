a8ef2a7a0b83370f3853d2ae966a86790365bfd5
MrNizzy <3134183631y@gmail.com>
2023-02-14 15:23:52 -0500

    Add: New parameters and code refactor

--- src/app/components/password/password.component.ts
+++ src/app/components/password/password.component.ts
@@ -8,29 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class PasswordComponent implements OnInit {
 
   length: number = 15;
+  useLowerCase: boolean = true;
   useUpperCase: boolean = false;
   useSpecialCharacters: boolean = false;
+  useNumbers: boolean = false;
   password: string = "";
   showCopied: boolean = false;
   lowerCaseChars: string = "abcdefghijklmnopqrstuvwxyz";
   upperCaseChars: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   specialChars: string = "!@#$%^&*_-,.";
+  numberChars: string = "0123456789";
   charSet: string = this.lowerCaseChars;
 
   ngOnInit() {
   }
   generatePassword() {
 
-    if (this.useUpperCase) {
-      this.charSet += this.upperCaseChars;
-    }
-    if (this.useSpecialCharacters) {
-      this.charSet += this.specialChars;
-    }
+    this.charSet =
+      (this.useLowerCase ? this.lowerCaseChars : '') +
+      (this.useUpperCase ? this.upperCaseChars : '') +
+      (this.useSpecialCharacters ? this.specialChars : '') +
+      (this.useNumbers ? this.numberChars : '');
 
-    for (let i = 0; i < this.length; i++) {
-      this.password += this.charSet.charAt(Math.floor(Math.random() * this.charSet.length));
-    }
+    this.password = ''
+
+    this.password = this.charSet ?
+      Array(this.length).fill(0)
+        .map(() => this.charSet
+          .charAt(
+            Math.floor(Math.random() * this.charSet.length)
+          )).join('')
+      : '';
+
   }
 
   copyPassword() {
